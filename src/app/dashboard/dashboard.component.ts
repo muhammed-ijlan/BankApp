f32071ebac85536543d3116448e7366667e19603
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-04-08 11:35:46 +0530

    dashboard styled succesfully

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  acno = '';
+  pswd = '';
+  amount = '';
 
-  constructor() { }
+  acno1 = '';
+  pswd1 = '';
+  amount1 = '';
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  deposit() {
+    alert('deposit clicked');
   }
-
+  withdraw() {
+    alert('withdraw clicked');
+  }
 }
