eebb4a4791c4ede2ebf6e0953fb1c9df98fa6d2d
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-04-17 14:00:52 +0530

    register form converted to reactive form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -12,15 +13,28 @@ export class RegisterComponent implements OnInit {
   acno = '';
   pswd = '';
 
-  constructor(private router: Router, private db: DataService) {}
+  //register form model
+  registerForm = this.fb.group({
+    uname: [''],
+    acno: [''],
+    pswd: [''],
+  });
+
+  constructor(
+    private router: Router,
+    private db: DataService,
+    private fb: FormBuilder
+  ) {}
 
   register() {
-    var acno = this.acno;
-    var pswd = this.pswd;
-    var uname = this.uname;
+    var acno = this.registerForm.value.acno;
+    var pswd = this.registerForm.value.pswd;
+    var uname = this.registerForm.value.uname;
     const result = this.db.register(uname, acno, pswd);
     if (result) {
       alert('succesfully register');
+      console.log(this.db.database);
+
       this.router.navigateByUrl('');
     } else {
       alert('account already exist');
